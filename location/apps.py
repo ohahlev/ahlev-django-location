8a358c1d00fa4f758e1867f8c9f8f35f1f918832
ahlev <ohahlev@gmail.com>
2019-12-30 22:38:54 +0700

    add version into verbose name

--- location/apps.py
+++ location/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.html import format_html
+from . import __version__ as VERSION
 
 class LocationConfig(AppConfig):
     name = 'location'
-    verbose_name = 'Location Management'
+    verbose_name = format_html("Location Management <span class='version'>{}</span>", VERSION)
     
